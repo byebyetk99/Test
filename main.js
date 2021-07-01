d1512c9bc0d5ceadfeda5d555906cf6476109eed
Quoc Le <levietquoc2711@gmail.com>
2021-07-01 23:04:13 +0700

    Test commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var fullName = 'Le Viet Quoc'
+var age = '26'
+
+var result = 'A' && 'B' && null && NaN && 'C'
+
+var result = null || 'C' || null || NaN || null 
+
+var chuoiTest = "Lê Viết Quốc sống ở số 28 Trần Thị Lý - "
++ "1. Lê Viết Quốc sống ở số 28 Trần Thị Lý - "
++ "2. Lê Viết Quốc sống ở số 28 Trần Thị Lý - "
++ "3. Lê Viết Quốc sống ở số 28 Trần Thị Lý - "
+;
+
+
+var firstName = 'Quoc'
+var lastName = 'Le'
+
+console.log('Toi ten: ' + firstName + ' ' + lastName);
+console.log(`Toi ten: ${firstName} ${lastName}`);
+
+
+console.log(chuoiTest);
+var myObject = {
+    name: 'Quoc',
+    age: 22,
+    adress: 'Quang Nam'
+}
+
+var abcd = new Number(3);
+console.log(abcd);
+
+console.log(result);
+
+
+var myFunction = function() {
+        Console.log(' Day la function');
+}
